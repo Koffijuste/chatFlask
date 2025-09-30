5967ceff19248624f336891f7f39d9fdc9bcc09e
Morning Star <koffioijuste@icloud.com>
2025-09-30 14:01:29 +0200

    Suppression de csrf dans index et app.py

--- app.py
+++ app.py
@@ -15,7 +15,6 @@ from flask_socketio import SocketIO, emit
 from models import db, User, Message
 from werkzeug.security import check_password_hash, generate_password_hash
 from werkzeug.utils import secure_filename
-from flask_wtf.csrf import CSRFProtect
 
 
 
@@ -24,8 +23,6 @@ app.config['SECRET_KEY'] = 'super-secret-chat-key-2025!'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['UPLOAD_FOLDER'] = 'static/uploads/avatars'
 
-# ✅ Activer la protection CSRF
-csrf = CSRFProtect(app)
 
 os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
 
